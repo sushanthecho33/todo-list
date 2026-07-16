b139441f0ceb8c5898416fd1ade7eab5a57efdf9
Your Name <you@example.com>
2026-07-16 22:27:48 +0530

    ain

---
+++ main.py
@@ -0,0 +1,20 @@
+tasks = []
+while True:
+    print("=================================")
+    print("      TO-DO LIST APPLICATION")
+    print("=================================")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Please select an option (1/2/3): ")
+    if choice == "1":
+        print("hello world")
+    elif choice == "2":
+        print("wow sheeesh")
+    elif choice == "3":
+        print("thank you")
+        break
+    else:
+        print("Invalid option. Try again.")
+        
